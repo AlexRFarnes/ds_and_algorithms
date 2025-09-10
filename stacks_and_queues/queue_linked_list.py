4aefc9048fe32d469862a07ce88f2b926c6ad09e
AlexRFarnes <alexrfarnes@gmail.com>
2025-09-10 17:37:12 +0800

    Double linked list based Queue

---
+++ stacks_and_queues/queue_linked_list.py
@@ -0,0 +1,26 @@
+# queue_linked_list.py
+
+
+class Node:
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+
+class Queue:
+    def __init__(self):
+        self.head = None  # dequeue from head
+        self.tail = None  # enqueue at tail
+        self.count = 0
+
+    def enqueue(self, data):
+        new_node = Node(data, None, None)
+        if self.head is None:  # Queue is empty
+            self.head = new_node
+            self.tail = self.head
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        self.count += 1
